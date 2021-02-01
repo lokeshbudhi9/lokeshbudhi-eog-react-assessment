98e274c4d198bf123dda4ec74f2b27046e354a9d
Lokesh Budhi <lokeshbudhi09@gmail.com>
2021-02-01 15:20:22 +0530

    feat: add getMultipleMeasurements query

---
+++ src/graphql/queries.js
@@ -0,0 +1,20 @@
+import {gql} from '@apollo/client';
+
+const GET_MULTIPLE_MEASUREMENTS = gql`
+  query GetMultipleMeasurements($input: [MeasurementQuery]) {
+    getMultipleMeasurements(input: $input) {
+        metric
+        measurements {
+          at
+          value
+          metric
+          unit
+        }
+    }
+  }
+`;
+  
+export {
+    GET_MULTIPLE_MEASUREMENTS
+}
+
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
+import { GET_MULTIPLE_MEASUREMENTS } from "../graphql/queries";
 
 const Dashboard = () => {
+    const { error, data, loading, refetch } = useQuery(GET_MULTIPLE_MEASUREMENTS, {
+        variables: {
+          input: [
+            {
+              "metricName": "injValveOpen",
+              "after": 1612164170093
+            }
+          ]
+        },
+      });
     return <div>The Dashboard</div>
 }
 
